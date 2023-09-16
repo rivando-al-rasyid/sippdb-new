eeb49250b1823c4b054b14a2946c25f8598b23cd
rivando al rasyid <nougat-tutorial@gmail.com>
2023-09-16 16:13:22 +0700

    update

--- app/Http/Controllers/DaftarController.php
+++ app/Http/Controllers/DaftarController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use RealRashid\SweetAlert\Facades\Alert;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use RealRashid\SweetAlert\Facades\Alert;
 
 // Load Models
 use App\Models\PenghasilanOrangtua;
@@ -15,16 +16,6 @@ use App\Models\Hasil;
 
 class DaftarController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         $hasil_ortu = PenghasilanOrangtua::all();
@@ -39,7 +30,7 @@ class DaftarController extends Controller
     {
         DB::beginTransaction();
 
-        $validator = \Validator::make($request->all(), [
+        $validator = Validator::make($request->all(), [
             'nama' => 'required',
             'agama' => 'required',
             'tanggal_lahir' => 'date|before:yesterday',
@@ -50,10 +41,10 @@ class DaftarController extends Controller
             'no_telp' => 'required',
             'nama_ayah' => 'required',
             'nama_ibu' => 'required',
-            'id_pekerjaan_ayah' => 'required|exists:tbl_pekerjaan_ortu,id',
-            'id_pekerjaan_ibu' => 'required|exists:tbl_pekerjaan_ortu,id',
-            'id_penghasilan_ayah' => 'required|exists:tbl_penghasilan_ortu,id',
-            'id_penghasilan_ibu' => 'required|exists:tbl_penghasilan_ortu,id',
+            'id_pekerjaan_ayah' => 'required|exists:pekerjaan_ortu,id',
+            'id_pekerjaan_ibu' => 'required|exists:pekerjaan_ortu,id',
+            'id_penghasilan_ayah' => 'required|exists:penghasilan_ortu,id',
+            'id_penghasilan_ibu' => 'required|exists:penghasilan_ortu,id',
             'no_telp_ortu' => 'required'
         ]);
 
@@ -61,11 +52,10 @@ class DaftarController extends Controller
             return redirect()->back()->withErrors($validator->errors());
         }
 
-        $data = [
+        $dataPeserta = [
             'nama' => $request->nama,
-            'id_jenis_kelamin' => $request->id_jenis_kelamin,
-            'id_agama' => $request->id_agama,
-            'id_jurusan' => $request->id_jurusan,
+            'jenis_kelamin' => $request->jenis_kelamin,
+            'agama' => $request->agama,
             'tanggal_lahir' => $request->tanggal_lahir,
             'tempat_lahir' => $request->tempat_lahir,
             'asal_sekolah' => $request->asal_sekolah,
@@ -76,14 +66,14 @@ class DaftarController extends Controller
             'id_penghasilan_ortu' => $request->id_penghasilan_ayah,
         ];
 
-        $daftar = PesertaPPDB::create($data);
+        $daftar = PesertaPPDB::create($dataPeserta);
         if (!$daftar) {
             DB::rollBack();
             Alert::error('Error', 'Please check your form again!');
             return redirect()->back();
         }
 
-        $data2 = [
+        $dataOrtu = [
             'id_peserta_ppdb' => $daftar->id,
             'id_pekerjaan_ayah' => $request->id_pekerjaan_ayah,
             'id_penghasilan_ayah' => $request->id_penghasilan_ayah,
@@ -94,18 +84,18 @@ class DaftarController extends Controller
             'no_tlp' => $request->no_telp_ortu
         ];
 
-        $ortu = BiodataOrtu::create($data2);
+        $ortu = BiodataOrtu::create($dataOrtu);
         if (!$ortu) {
             DB::rollBack();
             Alert::error('Error', 'Please check your form again!');
             return redirect()->back();
         }
 
-        $data3 = [
+        $dataHasil = [
             'nis' => $daftar->id
         ];
 
-        $hasil = Hasil::create($data3);
+        $hasil = Hasil::create($dataHasil);
         if (!$hasil) {
             DB::rollBack();
             Alert::error('Error', 'Please check your form again!');
@@ -113,7 +103,7 @@ class DaftarController extends Controller
         }
 
         DB::commit();
-        Alert::success('Success', 'Thank you for register!');
+        Alert::success('Success', 'Thank you for registering!');
         return redirect()->route('landing-page');
     }
 
